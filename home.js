212ddbfbc403776bb775d206d425a4c9ba3244d6
ridu101 <ridu116540@gmail.com>
2025-09-05 19:23:32 +0600

    button style with function done

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 // pre declared number, pin, bonus 
 const validNumber = 8801878710317;
 const validPin = 3119;
-const bonusCode= 'hiya50'
+const bonusCode = 'hiya50'
 
 function getAmountValue(id) {
     const value = document.getElementById(id).value;
@@ -22,6 +22,62 @@ function getAccountNumber(id) {
 }
 
 
+// toggling function handler
+function handleToggle(id) {
+    const events = document.getElementsByClassName('events');
+    for (const event of events) {
+        event.style.display = 'none';
+    }
+    document.getElementById(id).style.display = 'block';
+}
+
+// activation button style function
+function activateStyle(id){
+
+   const fromBtn = document.getElementsByClassName('forms-btn');
+        for (const btn of fromBtn) {
+            btn.classList.remove("border-[#0874f2]", "bg-[#0874f230]");
+            btn.classList.add("border-[#0874f20d]", "bg-[#f4f5f7]");
+        }
+
+        document.getElementById(id).classList.remove("border-[#0874f20d]", "bg-[#f4f5f7]");
+        document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f230]");
+
+}
+// toggling feature
+document.getElementById('add-money-btn')
+    .addEventListener('click', function () {
+        handleToggle('add-money-parent')
+        activateStyle('add-money-btn');
+        
+    })
+
+document.getElementById('cash-out-btn')
+    .addEventListener('click', function () {
+        handleToggle('cash-out-parent')
+        activateStyle('cash-out-btn');
+
+       
+    })
+
+document.getElementById('transfer-btn')
+    .addEventListener('click', function () {
+        handleToggle('transfer-container')
+         activateStyle('transfer-btn');
+
+    })
+document.getElementById('get-bonus-btn')
+    .addEventListener('click', function () {
+        handleToggle('bonus-container');
+         activateStyle('get-bonus-btn');
+    })
+document.getElementById('pay-button')
+    .addEventListener('click', function () {
+        handleToggle('pay-bill-container');
+         activateStyle('pay-button');
+    })
+
+//  add button event handler
 document.getElementById('btn-add')
     .addEventListener('click', function (e) {
         e.preventDefault()
@@ -29,7 +85,6 @@ document.getElementById('btn-add')
         const accountNumber = getAccountNumber('bank-account-number');
         const amountNumber = getAmountValue('add-amount');
         const pinNumber = getPinValue('pin-number');
-
         if (accountNumber != validNumber) {
             alert('Provide Correct Number')
             return;
@@ -38,49 +93,14 @@ document.getElementById('btn-add')
             alert('Provide Correct Pin');
             return;
         }
-
         // accessioning the available balance 
         const availableBalance = parseInt(document.getElementById('avail-balance').innerText);
-
         const totalBalance = amountNumber + availableBalance;
-
         document.getElementById('avail-balance').innerText = totalBalance;
 
 
     })
-// toggling feature
-document.getElementById('add-money-btn')
-    .addEventListener('click', function () {
-        document.getElementById('cash-out-parent').style.display = 'none';
-        document.getElementById('transfer-container').style.display = 'none';
-        document.getElementById('bonus-container').style.display='none';
-        document.getElementById('add-money-parent').style.display = 'block';
 
-    })
-document.getElementById('cash-out-btn')
-    .addEventListener('click', function () {
-        document.getElementById('add-money-parent').style.display = 'none';
-        document.getElementById('transfer-container').style.display = 'none';
-        document.getElementById('bonus-container').style.display='none';
-        document.getElementById('cash-out-parent').style.display = 'block';
-
-    })
-
-document.getElementById('transfer-btn')
-    .addEventListener('click', function () {
-
-        document.getElementById('cash-out-parent').style.display = 'none';
-        document.getElementById('add-money-parent').style.display = 'none';
-        document.getElementById('bonus-container').style.display='none';
-        document.getElementById('transfer-container').style.display = 'block';
-    })
-document.getElementById('get-bonus-btn')
-    .addEventListener('click', function () {
-        document.getElementById('cash-out-parent').style.display = 'none';
-        document.getElementById('add-money-parent').style.display = 'none';
-        document.getElementById('transfer-container').style.display = 'none';
-        document.getElementById('bonus-container').style.display='block';
-    })
 
 // withdraw button event handler
 
@@ -105,6 +125,7 @@ document.getElementById('withdraw-btn')
 
         document.getElementById('avail-balance').innerText = amountAfterWithdraw;
     })
+
 // send now button event handler
 
 document.getElementById('send-btn').addEventListener('click', function () {
@@ -131,18 +152,18 @@ document.getElementById('send-btn').addEventListener('click', function () {
 });
 
 // get bonus button event handler
-    document.getElementById('bonus-btn')
-        .addEventListener('click', function(){
-            const availableBalance= parseInt(document.getElementById('avail-balance').innerText);
-            const couponInput= document.getElementById('coupon-code-input').value;
-            
-            if (couponInput=== bonusCode){
-                const newBalance= availableBalance *50;
-                const totalBonus=newBalance/100;
-                const afterBonusTotalAmount= availableBalance+totalBonus;
-                document.getElementById('avail-balance').innerText= afterBonusTotalAmount;
-            }
-            else{
-                alert('⚠️Enter Correct Coupon Code !!')
-            }
-        })
+document.getElementById('bonus-btn')
+    .addEventListener('click', function () {
+        const availableBalance = parseInt(document.getElementById('avail-balance').innerText);
+        const couponInput = document.getElementById('coupon-code-input').value;
+
+        if (couponInput === bonusCode) {
+            const newBalance = availableBalance * 50;
+            const totalBonus = newBalance / 100;
+            const afterBonusTotalAmount = availableBalance + totalBonus;
+            document.getElementById('avail-balance').innerText = afterBonusTotalAmount;
+        }
+        else {
+            alert('⚠️Enter Correct Coupon Code !!')
+        }
+    })
