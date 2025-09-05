3325194206a61d5151e67dd65b4cad404f565684
ridu101 <ridu116540@gmail.com>
2025-09-05 12:57:14 +0600

    trasnfer section done

--- home.js
+++ home.js
@@ -1,29 +1,31 @@
 const validNumber = 8801878710317;
 const validPin = 3119;
 
-function getAmountValue(id){
-    const amountEntered= parseInt(document.getElementById(id).value);
-    return amountEntered;
-
+function getAmountValue(id) {
+    const value = document.getElementById(id).value;
+    const amountEntered = parseInt(value);
+    return amountEntered
 }
 
-function getPinValue (id){
-   
-    const pinEntered= parseInt(document.getElementById(id).value);
-    return pinEntered;
+function getPinValue(id) {
+    const value = document.getElementById(id).value;
+    const pinEntered = parseInt(value);
+    return pinEntered
 }
-function getAccountNumber(id){
-    const accountNumberEntered= parseInt(document.getElementById(id).value);
-    return accountNumberEntered;
 
+function getAccountNumber(id) {
+    const value = document.getElementById(id).value;
+    const accountNumberEntered = parseInt(value);
+    return accountNumberEntered
 }
+
 
 document.getElementById('btn-add')
     .addEventListener('click', function (e) {
         e.preventDefault()
         const bank = document.getElementById('bank').value;
-        const accountNumber =getAccountNumber('bank-account-number');
-        const amountNumber =getAmountValue('add-amount');
+        const accountNumber = getAccountNumber('bank-account-number');
+        const amountNumber = getAmountValue('add-amount');
         const pinNumber = getPinValue('pin-number');
 
         if (accountNumber != validNumber) {
@@ -48,15 +50,26 @@ document.getElementById('btn-add')
 document.getElementById('add-money-btn')
     .addEventListener('click', function () {
         document.getElementById('cash-out-parent').style.display = 'none';
+        document.getElementById('transfer-container').style.display = 'none';
         document.getElementById('add-money-parent').style.display = 'block';
 
     })
 document.getElementById('cash-out-btn')
     .addEventListener('click', function () {
         document.getElementById('add-money-parent').style.display = 'none';
+        document.getElementById('transfer-container').style.display = 'none';
         document.getElementById('cash-out-parent').style.display = 'block';
 
     })
+
+document.getElementById('transfer-btn')
+    .addEventListener('click', function () {
+
+        document.getElementById('cash-out-parent').style.display = 'none';
+        document.getElementById('add-money-parent').style.display = 'none';
+        document.getElementById('transfer-container').style.display = 'block';
+    })
+
 // withdraw button event handler
 
 document.getElementById('withdraw-btn')
@@ -71,12 +84,36 @@ document.getElementById('withdraw-btn')
         }
         if (pinNumber != validPin) {
             alert('Enter valid pin')
-        } 
-        if( availableBalance < withdrawAmount){
+        }
+        if (availableBalance < withdrawAmount) {
             alert('Insufficient Balance');
-            return ;
+            return;
         }
         const amountAfterWithdraw = availableBalance - withdrawAmount;
 
         document.getElementById('avail-balance').innerText = amountAfterWithdraw;
     })
+// send now button event handler
+
+document.getElementById('send-btn').addEventListener('click', function(){
+    const availableBalance = parseInt(document.getElementById('avail-balance').innerText);
+    const transferAmount = getAmountValue('transfer-amount');
+    const accountNumber = getAccountNumber('user-number');
+    const pinNumber = getPinValue('pin-number-transfer'); // correct id
+
+    if (accountNumber != validNumber) {
+        alert('Enter valid account Number');
+        return;
+    }
+    if (pinNumber != validPin) {
+        alert('Enter valid pin');
+        return;
+    }
+    if (availableBalance < transferAmount) {
+        alert('Insufficient Balance');
+        return;
+    }
+
+    const amountAfterTransfer = availableBalance - transferAmount;
+    document.getElementById('avail-balance').innerText = amountAfterTransfer;
+});
